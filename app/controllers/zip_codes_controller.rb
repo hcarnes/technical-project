6c4e467b4da83160c929324fdf2c251f74f776dd
Hannah Carnes <hecarnes@gmail.com>
2019-07-17 16:30:07 -0400

    Redirect to root path if search doesn't include a zipcode

--- app/controllers/zip_codes_controller.rb
+++ app/controllers/zip_codes_controller.rb
@@ -7,13 +7,17 @@ class ZipCodesController < ApplicationController
   def search
     center_zip = ZipCode.find_by(zip_code: params[:zip_code])
     
-    @zip_codes = ZipCode.where([
-      "ST_Distance(ST_GeomFromText(?, 4326), zip_codes.location, 'Statute mile') < ?",
-      "POINT(#{center_zip.longitude} #{center_zip.latitude})",
-      params[:radius].to_i
-    ])
+    if center_zip.present?
+      @zip_codes = ZipCode.where([
+        "ST_Distance(ST_GeomFromText(?, 4326), zip_codes.location, 'Statute mile') < ?",
+        "POINT(#{center_zip.longitude} #{center_zip.latitude})",
+        params[:radius].to_i
+      ])
 
-    render :index
+      render :index
+    else
+      redirect_to root_path
+    end
   end
 
 end
